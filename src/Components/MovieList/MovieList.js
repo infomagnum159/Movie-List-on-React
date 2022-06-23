7d991f3fbc0973d4fe30f0d7cecd9e6cb0e9b0cc
infomagnum159 <infomagnum159@gmail.com>
2022-06-23 15:44:10 +0600

    Imported and added styles to Inputs and Buttons

--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './MovieList.css'
 
 class MovieList extends Component {
 
--- src/Components/MovieName/MovieName.js
+++ src/Components/MovieName/MovieName.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './MovieName.css'
 
 class MovieName extends Component {
 
